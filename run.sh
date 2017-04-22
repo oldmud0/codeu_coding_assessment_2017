701de5d43d5c04cf9f78c6e2c4c8b6d2f8a54248
oldmud0 <oldmud0@users.noreply.github.com>
2017-04-21 21:36:00 -0500

    Changed run path in run.sh

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-java -cp bin com.google.codeu.codingchallenge.TestMain
+java -cp bin com.google.codeu.codingchallenge.tests.TestMain
