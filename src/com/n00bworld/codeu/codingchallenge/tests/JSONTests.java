5f49fed9af17c1cffe985dbe9577e88f1e25e6c4
oldmud0 <oldmud0@users.noreply.github.com>
2017-04-22 22:57:59 -0500

    Screw it, let's switch to JUnit. More tests, more fun eh?

---
+++ src/com/n00bworld/codeu/codingchallenge/tests/JSONTests.java
@@ -0,0 +1,14 @@
+package com.n00bworld.codeu.codingchallenge.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JSONTests {
+
+    @Test
+    public void testParse() {
+        fail("Not yet implemented");
+    }
+
+}
